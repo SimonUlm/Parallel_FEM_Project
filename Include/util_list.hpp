1be29de64f88cf2c88870f37f13ebc196130aa3f
simonakis <simon.deutrich@uni-ulm.de>
2023-05-27 12:00:46 +0200

    [ADD] Added ListIterator

--- Include/util_list.hpp
+++ Include/util_list.hpp
@@ -5,50 +5,93 @@
 
 
 namespace Util {
+
+    template<typename List>
+    class ListIterator {
+    public:
+        using ValueType = typename List::ValueType;
+
+        ListIterator(ValueType *ptr) :
+                ptr_(ptr) {}
+
+        ListIterator &operator++() {
+            ++ptr_;
+            return *this;
+        }
+
+        ValueType &operator*() {
+            return *ptr_;
+        }
+
+        bool operator!=(const ListIterator &other) const {
+            return ptr_ != other.ptr_;
+        }
+
+    private:
+        ValueType *ptr_;
+    };
+
     template<typename T>
     class List {
-    private:
-        T *data;
-
     public:
+        using ValueType = T;
+        using Iterator = ListIterator<List<T>>;
+
         long count;
 
         explicit List() :
-                count(0), data(nullptr) {
+                count(0), data_(nullptr) {
         }
+
         explicit List(long count) :
-                count(count), data(nullptr) {
+                count(count), data_(nullptr) {
             if (count != 0)
-                data = new T[count];
+                data_ = new T[count];
         }
+
         ~List() {
-            delete [] data;
+            delete[] data_;
         }
-        List(List && other) noexcept:
-                data(other.data), count(other.count) {
-            other.data = nullptr;
+
+        List(List &&other) noexcept:
+                data_(other.data_), count(other.count) {
+            other.data_ = nullptr;
             other.count = 0;
         }
+
         List(const List &) = delete;
 
-        List & operator=(List &&other) noexcept {
-            delete [] data;
-            data = other.data;
+        List &operator=(List &&other) noexcept {
+            delete[] data_;
+            data_ = other.data_;
             count = other.count;
-            other.data = nullptr;
+            other.data_ = nullptr;
             other.count = 0;
             return *this;
         }
-        List & operator=(const List &) = delete;
 
-        T & operator()(long index) const {
+        List &operator=(const List &) = delete;
+
+        T &operator()(long index) const {
             assert(index < count || index == 0);
-            return data[index];
+            return data_[index];
         }
-        T & operator()(long index) {
+
+        T &operator()(long index) {
             assert(index < count || index == 0);
-            return data[index];
+            return data_[index];
         }
+
+        Iterator begin() {
+            return Iterator(data_);
+        }
+
+        Iterator end() {
+            return Iterator(data_ + count);
+        }
+
+    private:
+        T *data_;
     };
 }
 
